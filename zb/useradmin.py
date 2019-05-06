ab3ccdae229663ab762076694f79dcd07ca8f9cb
Cooper Liu <shan275@163.com>
2019-05-06 18:31:37 +0800

    修改stradmin不带参数挂死的问题

--- zb/useradmin.py
+++ zb/useradmin.py
@@ -119,9 +119,11 @@ class UserHomeView(admin.AdminIndexView):
                 logger.info(param)
                 if param.has_key('user'):
                     para_dict = dict(user=param['user'][0])
-                    logger.info(para_dict)
-                    url = urlAddPara(request.url, para_dict)
-                    return redirect(url)
+                else:
+                    para_dict = dict(user='0')
+                logger.info(para_dict)
+                url = urlAddPara(request.url, para_dict)
+                return redirect(url)
         # 获取表项数量
         stat = libredis.LibRedis(userId).hashGetAll('g_stat')
         tasks = libcommon.getUserTaskList(userId)
